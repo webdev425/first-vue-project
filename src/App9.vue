16df848a40aff8295e6d0e0d038ef61abd6131f2
webdev425 <thanh.dev425@gmail.com>
2023-02-24 00:51:44 +0800

    lifecycle and template refs

---
+++ src/App9.vue
@@ -0,0 +1,13 @@
+<script>
+  export default {
+    mounted() {
+      setTimeout(() => {
+        this.$refs.p.textContent = "mounted"
+      }, 3000)
+    }
+  }
+</script>
+
+<template>
+  <p ref="p">Hello</p>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './App7.vue'
+import App from './App9.vue'
 
 createApp(App).mount('#app')
