1fcb9e0a6c5c82719c7ba6341bf722caff487a62
webdev425 <thanh.dev425@gmail.com>
2023-02-24 01:01:07 +0800

    watchers

---
+++ src/App10.vue
@@ -0,0 +1,23 @@
+<script>
+  export default {
+    data() {
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      add() {
+        this.count++
+      }
+    },
+    watch: {
+      count() {
+        console.log(this.count)
+      }
+    }
+  }
+</script>
+
+<template>
+  <button @click="add">count is {{count}}</button>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './App9.vue'
+import App from './App10.vue'
 
 createApp(App).mount('#app')
