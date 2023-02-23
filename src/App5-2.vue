f3e2c62248c57dc4a97680e102863d62e11f9e6b
webdev425 <thanh.dev425@gmail.com>
2023-02-24 00:30:50 +0800

    form bindings

---
+++ src/App5-2.vue
@@ -0,0 +1,14 @@
+<script>
+  export default {
+    data() {
+      return {
+        text: '',
+      }
+    }
+  }
+</script>
+
+<template>
+  <input v-model="text" />
+  <p>{{text}}</p>
+</template>
